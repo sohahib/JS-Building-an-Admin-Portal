fd049ff1ee7814d435733d1b13ebd1e073a2f9fc
sohahib <syaoda@yahoo.fr>
2023-06-04 22:52:43 -0400

    done

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
 
 // Your Code Here
+async function getData(){
+    let result =  await fetch('http://localhost:3001/listBooks')
+    let books =  await result.json()
+    
+    books.forEach(book =>{
+        let bookList = document.getElementById('root')
+    
+        let list = document.createElement('li')
+        list.textContent = book.title
+    
+        let textInput = document.createElement('input')
+        textInput.value = book.quantity
+    
+        let saveButton = document.createElement('button')
+        saveButton.textContent = 'Save'
+        saveButton.style.border = 'none'
+        
+        saveButton.addEventListener('click', ()=>{
+            fetch('http://localhost:3001/updateBook',{
+                method:'PATCH',
+                headers:{
+                    'conten-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    id: book.id,
+                    quantity: textInput.value
+                })
+            })
+        })
+
+        list.append(textInput, saveButton)
+        bookList.append(list);
+    })
+}
+getData()
+
+
+
+// function renderBooks(book){
+   
+
+// }
